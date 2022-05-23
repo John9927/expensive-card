6e9d16833fc2fbf3babfd186a1bf67c8ecd90efb
Giovanni Izzi <giovanni9972@libero.it>
2022-05-23 09:29:28 +0200

    add chart

---
+++ src/app/components/chart/chart.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-chart',
+  templateUrl: './chart.component.html',
+  styleUrls: ['./chart.component.scss']
+})
+export class ChartComponent implements OnInit {
+
+  data = [
+    {
+      "day": "mon",
+      "amount": 17.45
+    },
+    {
+      "day": "tue",
+      "amount": 34.91
+    },
+    {
+      "day": "wed",
+      "amount": 52.36
+    },
+    {
+      "day": "thu",
+      "amount": 31.07
+    },
+    {
+      "day": "fri",
+      "amount": 23.39
+    },
+    {
+      "day": "sat",
+      "amount": 43.28
+    },
+    {
+      "day": "sun",
+      "amount": 25.48
+    }
+  ]
+
+  datas: any;
+  amount$ = '921.48';
+  amount = '30px';
+
+  constructor() {
+    this.datas = this.data;
+  }
+
+  ngOnInit(): void { }
+
+}
